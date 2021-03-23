d9bc3655e25290159386a3257649e828d75fe328
Samantha Jordan <shinyguns@gmail.com>
2021-03-22 19:29:56 -0500

    done, implementation left

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+  //get form
+  let addToList = document.querySelector('#create-task-form')
+  //get list items
+  let theListItem = document.querySelector('#tasks')
+  // get list
+  let theList = document.querySelector('#list')
+
+
+  //listen for submit 
+  addToList.addEventListener('submit', function(event){
+    //do not submit form
+    event.preventDefault()
+    // //if empty ignore it
+    // if (addToList.nodeValue.length < 1) return;
+    //newTask value
+    let newItem = document.querySelector('#new-task-description').value
+    theList.innerHTML += `<li> ${newItem} </li>`;
+  })
+}, false);
